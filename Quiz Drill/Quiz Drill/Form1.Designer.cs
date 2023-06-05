aacdf0a2315524a3cf622f75f4a5fe0be5fd580f
Lesedi61 <108560986+Lesedi61@users.noreply.github.com>
2023-06-05 09:53:37 +0200

    Add files via upload

---
+++ Quiz Drill/Quiz Drill/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace Quiz_Drill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumA = new System.Windows.Forms.Label();
+            this.logiOper = new System.Windows.Forms.Label();
+            this.lblNumB = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.TextBox();
+            this.lblSubmit = new System.Windows.Forms.Button();
+            this.lblQuesNum = new System.Windows.Forms.Label();
+            this.lblComplete = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arithmetic Drills For Kids";
+            // 
+            // lblNumA
+            // 
+            this.lblNumA.AutoSize = true;
+            this.lblNumA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumA.Location = new System.Drawing.Point(70, 107);
+            this.lblNumA.Name = "lblNumA";
+            this.lblNumA.Size = new System.Drawing.Size(36, 25);
+            this.lblNumA.TabIndex = 1;
+            this.lblNumA.Text = "00";
+            // 
+            // logiOper
+            // 
+            this.logiOper.AutoSize = true;
+            this.logiOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logiOper.Location = new System.Drawing.Point(141, 107);
+            this.logiOper.Name = "logiOper";
+            this.logiOper.Size = new System.Drawing.Size(25, 25);
+            this.logiOper.TabIndex = 2;
+            this.logiOper.Text = "+";
+            // 
+            // lblNumB
+            // 
+            this.lblNumB.AutoSize = true;
+            this.lblNumB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumB.Location = new System.Drawing.Point(226, 107);
+            this.lblNumB.Name = "lblNumB";
+            this.lblNumB.Size = new System.Drawing.Size(36, 25);
+            this.lblNumB.TabIndex = 3;
+            this.lblNumB.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(309, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "=";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Lime;
+            this.lblStatus.Location = new System.Drawing.Point(70, 169);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(97, 29);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "correct";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.Location = new System.Drawing.Point(370, 100);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(100, 36);
+            this.lblAnswer.TabIndex = 6;
+            this.lblAnswer.TextChanged += new System.EventHandler(this.CheckAnswer);
+            // 
+            // lblSubmit
+            // 
+            this.lblSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubmit.Location = new System.Drawing.Point(562, 101);
+            this.lblSubmit.Name = "lblSubmit";
+            this.lblSubmit.Size = new System.Drawing.Size(75, 35);
+            this.lblSubmit.TabIndex = 7;
+            this.lblSubmit.Text = "Submit";
+            this.lblSubmit.UseVisualStyleBackColor = true;
+            this.lblSubmit.Click += new System.EventHandler(this.CheckBtnSubmit);
+            // 
+            // lblQuesNum
+            // 
+            this.lblQuesNum.AutoSize = true;
+            this.lblQuesNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuesNum.Location = new System.Drawing.Point(12, 107);
+            this.lblQuesNum.Name = "lblQuesNum";
+            this.lblQuesNum.Size = new System.Drawing.Size(24, 25);
+            this.lblQuesNum.TabIndex = 8;
+            this.lblQuesNum.Text = "0";
+            // 
+            // lblComplete
+            // 
+            this.lblComplete.AutoSize = true;
+            this.lblComplete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplete.ForeColor = System.Drawing.Color.Lime;
+            this.lblComplete.Location = new System.Drawing.Point(70, 231);
+            this.lblComplete.Name = "lblComplete";
+            this.lblComplete.Size = new System.Drawing.Size(145, 29);
+            this.lblComplete.TabIndex = 9;
+            this.lblComplete.Text = "Completed";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.Lime;
+            this.lblScore.Location = new System.Drawing.Point(70, 283);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(84, 29);
+            this.lblScore.TabIndex = 10;
+            this.lblScore.Text = "Score";
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercent.ForeColor = System.Drawing.Color.Lime;
+            this.lblPercent.Location = new System.Drawing.Point(70, 332);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(152, 29);
+            this.lblPercent.TabIndex = 11;
+            this.lblPercent.Text = "Percentage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Questions";
+            // 
+            // lblRestart
+            // 
+            this.lblRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRestart.Location = new System.Drawing.Point(75, 390);
+            this.lblRestart.Name = "lblRestart";
+            this.lblRestart.Size = new System.Drawing.Size(91, 31);
+            this.lblRestart.TabIndex = 13;
+            this.lblRestart.Text = "Restart";
+            this.lblRestart.UseVisualStyleBackColor = true;
+            this.lblRestart.Click += new System.EventHandler(this.lblRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 480);
+            this.Controls.Add(this.lblRestart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblComplete);
+            this.Controls.Add(this.lblQuesNum);
+            this.Controls.Add(this.lblSubmit);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNumB);
+            this.Controls.Add(this.logiOper);
+            this.Controls.Add(this.lblNumA);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumA;
+        private System.Windows.Forms.Label logiOper;
+        private System.Windows.Forms.Label lblNumB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox lblAnswer;
+        private System.Windows.Forms.Button lblSubmit;
+        private System.Windows.Forms.Label lblQuesNum;
+        private System.Windows.Forms.Label lblComplete;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button lblRestart;
+    }
+}
+
---
+++ Quiz Drill/Quiz Drill/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Drill
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        string [] Maths = { "Add", "Subtract", "Multiple", "Division" };
+        int total,quesNum=0;
+        int score=0;
+        public Form1()
+        {
+            InitializeComponent();
+            lblStatus.Text = null;
+            lblComplete.Text = null;
+            lblPercent.Text = null;
+            lblScore.Text = null;
+            lblRestart.Visible = false;
+            SetupApplication();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(lblAnswer.Text, "[^0-9-]")) // Input Validation
+            {
+                MessageBox.Show("Please Enter Numbers Only");
+                lblAnswer.Text = lblAnswer.Text.Remove(lblAnswer.Text.Length - 1); // Erase Invalid Value
+
+            }
+        }
+
+        private void CheckBtnSubmit(object sender, EventArgs e)
+        {
+            
+            if (lblAnswer.TextLength == 0) // Check if textfield is empty
+            {
+                MessageBox.Show("Please Enter Your Answer");
+            }
+            else
+            {
+                int userAnswer = Convert.ToInt32(lblAnswer.Text);
+                if (userAnswer == total) // Check if user entered correct answer
+                {
+                    lblStatus.Text = "Correct";
+                    lblStatus.ForeColor = Color.Green;
+                    score += 1;
+                    SetupApplication();
+                }
+                else
+                {
+                    lblStatus.Text = "Incorrect, Correct Answer Is "+total.ToString(); // Display correct answer
+                    lblStatus.ForeColor = Color.Red;
+                    SetupApplication();
+                }
+            }
+            if (quesNum > 10)
+            {
+              
+                lblScore.Text = "Score : "+score.ToString() + "/10"; // Display score
+                lblComplete.Text = "You Have Completed Your Quiz Successfully";
+                lblSubmit.Enabled = false; // Disable submit button after completng quiz
+                lblRestart.Visible = true; // Show Restart button after completion
+
+            }
+            
+        }
+
+        private void lblRestart_Click(object sender, EventArgs e)
+        {
+            // Restart application
+            quesNum = 0;
+            score = 0;
+            lblSubmit.Enabled = true;
+            lblRestart.Visible = false;
+            lblStatus.Text = null;
+            lblComplete.Text = null;
+            lblPercent.Text = null;
+            lblScore.Text = null;
+            lblRestart.Visible = false;
+
+            SetupApplication();
+        }
+
+        private void SetupApplication()
+        {
+            int numA=0;
+            int numB=0;
+
+            lblAnswer.Text = null; // Erase previous answer
+
+            switch (Maths[rnd.Next(0, Maths.Length)])
+            {
+                case "Add":
+                    numA = rnd.Next(0, 50); // Generate random values
+                    numB = rnd.Next(0, 50);
+                    total = numA + numB;
+                    logiOper.Text = "+";
+                    logiOper.ForeColor = Color.DarkGreen;
+                    break;
+                case "Subtract":
+                    numA = rnd.Next(0, 50);
+                    numB = rnd.Next(0, 50);
+                    total = numA - numB;
+                    logiOper.Text = "-";
+                    logiOper.ForeColor = Color.Maroon;
+                    break;
+                case "Multiple":
+                    numA = rnd.Next(0, 12);
+                    numB = rnd.Next(0, 12);
+                    total = numA * numB;
+                    logiOper.Text = "x";
+                    logiOper.ForeColor = Color.Purple;
+                    break;
+                case "Division":
+                    numA = rnd.Next(0, 13);
+                    numB = rnd.Next(0, 13);
+                    total = numA / numB;
+                    logiOper.Text = "/";
+                    logiOper.ForeColor = Color.Blue;
+                    break;
+            }
+            lblNumA.Text = numA.ToString();
+            lblNumB.Text = numB.ToString();
+            quesNum += 1;
+            lblQuesNum.Text = quesNum.ToString()+".";
+             
+        }
+    }
+}
